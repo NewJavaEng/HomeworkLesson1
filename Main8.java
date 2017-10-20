287e7851ab1a1e5b8041da822302e44b60de9ade
NewJavaEng <32841342+NewJavaEng@users.noreply.github.com>
2017-10-20 13:24:05 +0300

    Add files via upload

---
+++ Main8.java
@@ -0,0 +1,18 @@
+//Lesson 1 Homework Task 9
+public class Main8 {
+	
+	public static void main (String[] args) {
+		
+		int number;
+		int summ = 0;
+		
+		for (number=0;number<=100;number++) {
+			summ+=number;
+			
+		}
+		
+		System.out.println("Summ of numbers from 0 to 100 is " + summ);
+		
+	}
+	
+}
